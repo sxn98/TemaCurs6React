89a54f3d9e93419e5e6995ff6707a6e8e6438f66
sxn98 <sxn9898@gmail.com>
2022-07-31 19:17:11 +0300

    o tentativa la tabla

--- tema6/src/App.js
+++ tema6/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table className="tabla">
+      <tr >
+        <td >X</td>
+        <td>O</td>
+        <td>X</td>
+      </tr>
+      <tr >
+        <td>O</td>
+        <td>X</td>
+        <td>O</td>
+      </tr>
+      <tr>
+        <td>X</td>
+        <td>O</td>
+        <td>X</td>
+      </tr>
+    </table>
   );
 }
 
